f3d56f60490e0d56908ad94073ed53180c87bc45
JayYu <jcyu56622@gmail.com>
2021-01-09 14:53:39 +0900

    버튼 누르면 2개씩 증가할 수 있도록 수정 이슈 해결!!

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -29,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      ++_counter;
     });
   }
 
