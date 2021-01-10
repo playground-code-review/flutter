ecb0f906b78cbff0dc629d362f8a353afb75b97e
wonpyoJang <jangwonpyo0@gmail.com>
2021-01-11 01:56:17 +0900

    [added] 카운터 2 감소하도록 하는 마이너스 버튼 추가
    화면 우측 하단에 빨간색 마이너스 버튼을 추가하였습니다.
    해당 버튼을 누를 경우 counter가 2씩 감소하게 됩니다.
    
    카운터는 마이너스 범위의 값도 가질 수 있습니다.
    
    카운터 버튼을 누를 경우 카운터를 증가시키기 위해 _decrementCounter 함수를
    추가했습니다.

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -32,6 +32,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter -= 2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -76,10 +82,25 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            backgroundColor: Colors.blue,
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            backgroundColor: Colors.red,
+            child: Icon(Icons.remove),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
