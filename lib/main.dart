178754294a24fc72c31e7eb05d8d3d88b2ac3cec
wonpyoJang <jangwonpyo0@gmail.com>
2021-01-11 02:14:38 +0900

    [fixed] 앱바 타이틀 수정
    홈 위젯의 앱바 타이틀을 PlayGround로 변경합니다.

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'PlayGround'),
     );
   }
 }
