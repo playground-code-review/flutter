0a2078af5daccb0071569ac03bf5d886d71a5d90
wonpyoJang <jangwonpyo0@gmail.com>
2021-01-12 02:30:02 +0900

    [test] clamp, reset에 대한 테스트 추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,14 +27,69 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('2'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.text('10'), findsOneWidget);
+  });
+
+  testWidgets('Counter decrements smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '-' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
     await tester.tap(find.byIcon(Icons.remove));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // Verify that our counter has dcremented
     expect(find.text('0'), findsOneWidget);
     expect(find.text('2'), findsNothing);
 
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('2'), findsNothing);
+  });
+
+  testWidgets('Counter reset test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Tap the '-' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('4'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+
+    // Tap the 'reset' icon and trigger a frame.
+    await tester.tap(find.text('RESET'));
+    await tester.pump();
+
+    // Verify that our counter has benn reset.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('-2'), findsNothing);
+  });
+
+  testWidgets('check appbar title', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    await tester.pump();
     // AppBar 제목 확인
     expect(find.text('PlayGround'), findsOneWidget);
   });
