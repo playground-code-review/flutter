58469fba2084cd5aef30c160961c8ba428d2af25
Aqudi <gjdigj145@gmail.com>
2021-01-11 17:25:36 +0900

    feature: Add Increment 2 and Decrement 2 buttons

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -21,17 +21,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -76,11 +65,23 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: Wrap(
+        spacing: 5,
+        children: [
+          _buildButton(2, Icons.add),
+          _buildButton(-2, Icons.remove),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildButton(number, iconData) {
+    return FloatingActionButton(
+      onPressed: () => setState(() {
+        _counter += number;
+      }),
+      tooltip: 'Increment 2',
+      child: Icon(iconData),
     );
   }
 }
